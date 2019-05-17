2d5ad44d39b3f53caaa24a95dff5b365b560c8b5
JackH17 <jack.hawkins77@googlemail.com>
2019-05-17 10:53:10 +0200

    all tests passsing

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,46 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_reader :name, :roster
+
+
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+  # this works as well.
+  #   if @roster.has_key?(grade)
+  #   @roster[grade] << name
+  # else
+  #   @roster[grade] = [name]
+  #   end
+  # end
+    roster[grade] ||=[]
+
+    # if roster[grade] already exists leave it alone.
+    # if it doesnt set roster[grade] = []
+
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+
+  def sort
+    roster.each do |grade, students|
+      students.sort!
+    end
+  end
+
+
+
+end
